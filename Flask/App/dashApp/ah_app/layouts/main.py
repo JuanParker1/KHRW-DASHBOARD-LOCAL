16ece2d9e152e9c8ef34e93b6571a98f96a94d91
Pooya Shirazi <p.shirazi.a@gmail.com>
2021-05-11 05:05:51 +0430

    Tab 3 Complited

--- Flask/App/dashApp/ah_app/layouts/main.py
+++ Flask/App/dashApp/ah_app/layouts/main.py
@@ -16,13 +16,13 @@ TAB_PAN = html.Div(
             """
                     <ul class="nav nav-tabs mt-1" role="tablist">
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#Tab_1">ورود داده‌ها</a>
+                            <a class="nav-link active" data-toggle="tab" href="#Tab_1">ورود داده‌ها</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" data-toggle="tab" href="#Tab_2">چاه‌های مشاهده‌ای</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" data-toggle="tab" href="#Tab_3">هیدروگراف آبخوان</a>
+                            <a class="nav-link" data-toggle="tab" href="#Tab_3">هیدروگراف آبخوان</a>
                         </li>
                     </ul>
             """
@@ -36,7 +36,7 @@ TAB_PAN = html.Div(
                     children=[
                         TAB_1
                     ],
-                    className="tab-pane fade",
+                    className="tab-pane active",
                     id="Tab_1"
                 ),
                 html.Div(
@@ -50,7 +50,7 @@ TAB_PAN = html.Div(
                     children=[
                         TAB_3
                     ],
-                    className="tab-pane active",
+                    className="tab-pane fade",
                     id="Tab_3"
                 ),
             ],
